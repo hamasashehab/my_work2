2661f2d0d4b184b5f2f3d3dba64792cb373173d2
Ahmed Shehab <ahmedshahab2468@gmail.com>
2024-07-17 00:52:56 +0300

    Add files via upload

---
+++ home page/script.js
@@ -0,0 +1,8 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+const nav = document.getElementById("nav");
+menuIcon.addEventListener('click',()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+    
+});
